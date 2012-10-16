c15d567aaf3aa020805c768781eddc4c2c6b545e
tycho garen <garen@tychoish.com>
2012-10-16 00:35:21 -0400

    aggregator: working proof of concept for aggregator program

--- csa.py
+++ csa.py
@@ -21,27 +21,82 @@ Notes:
 """
 
 import yaml
+import argparse
 
 def cli():
-    import argparse
-
     parser = argparse.ArgumentParser('build rst page from a yaml spec')
     parser.add_argument('input', nargs='?', help='specify input file.' )
+    parser.add_argument('output', nargs='?', default=None, help='specify output file.' )
 
     return parser.parse_args()
 
 def parse_page(input):
     page = open(input, 'r').read()
+    return yaml.load_all(page).next()
 
-    parsed = yaml.load(page)
+def render_title(title):
+    output = []
 
-    print parsed
+    lines = '=' * len(title)
+    output.append(lines)
+    output.append(title)
+    output.append(lines)
+    output.append('')
 
+    return output
+
+def render_includes(pages):
+    output = []
+
+    for page in pages:
+        output.append('.. include:: ' + page)
+        meta = yaml.load(open(page.rsplit('.', 1)[0] + '.yaml', 'r').read())
+        if 'header' in meta:
+            output.append('   :start-line: ' + str(meta['doc_start']))
+        output.append('')
+
+    return output
+
+def render_archive(pages):
+    output = []
+
+    for page in pages:
+        meta = yaml.load(open(page.rsplit('.', 1)[0] + '.yaml', 'r').read())
+        output.append('- `' + meta['title'] + ' </' + meta['output'] + '>' + '`_')
+
+    output.append('')
+
+    return output
+
+def render(definition):
+    output = []
+    output.extend(render_title(definition['title']))
+
+    output.append(definition['preamble'])
+    output.append('')
+
+    if definition['type'] == 'include':
+        output.extend(render_includes(definition['pages']))
+    elif definition['type'] == 'archive':
+        output.extend(render_archive(definition['pages']))
+
+    output.append(definition['postamble'])
+
+    return output
 
 def main():
     interface = cli()
 
-    parse_page(interface.input)
+    if interface.output is None:
+        output_file = interface.input.rsplit('.', 1)[0] + '.rst'
+    else:
+        output_file = interface.output
+
+    output = open(output_file, 'w')
+
+    for line in render(parse_page(interface.input)):
+        output.write(line)
+        output.write('\n')
 
 if __name__ == '__main__':
     main()
