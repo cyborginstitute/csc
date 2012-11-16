df3e9b63a818e3e75da1f08a76ee40bc241f9ff3
tycho garen <garen@tychoish.com>
2012-11-16 16:32:37 -0500

    more csa aggregation work

--- csa.py
+++ csa.py
@@ -7,39 +7,31 @@ import sys
 
 DEFAULT_EXTENSION = 'rst'
 
-def parse_page(input):
-    return yaml.load_all(open(input, 'r').read()).next()
-
-def parse_meta(input_page, yaml_extension='yaml'):
-    return yaml.load(open(input_page.rsplit('.', 1)[0] + '.' + yaml_extension, 'r').read())
-
-def __init__(self):
-
 def render_title(title):
     line = '=' * len(title)
     return [ line , title , line ]
 
-def render_includes(pages):
+def render_content(pages, output_type='archive'):
     output = []
 
-    for page in pages:
-        output.append('.. include:: ' + page)
-        meta = parse_meta(page)
-        if 'header' in meta:
-            output.append('   :start-line: ' + str(meta['doc_start']))
+    if output_type = 'archive': 
+
+        for page in pages:
+            meta = parse_meta(page)
+            output.append('- `' + meta['title'] + ' </' + meta['output'] + '>' + '`_')
 
         output.append('')
 
-    return output
+    elif output_type = 'includes': 
 
-def render_archive(pages):
-    output = []
+        for page in pages:
+            output.append('.. include:: ' + page)
+            meta = parse_meta(page)
 
-    for page in pages:
-        meta = parse_meta(page)
-        output.append('- `' + meta['title'] + ' </' + meta['output'] + '>' + '`_')
+            if 'header' in meta:
+                output.append('   :start-line: ' + str(meta['doc_start']))
 
-    output.append('')
+            output.append('')
 
     return output
 
@@ -61,16 +53,17 @@ def render_directory_archive(directory, output_type='archive', input_extension=D
         return None
 
 def render(definition):
-    output = [ render_title(definition['title']), definition['preamble'], '' ]
+    output = render_title(definition['title'])
 
-    if definition['type'] == 'include':
-        output.extend(render_includes(definition['pages']))
-    elif definition['type'] == 'archive':
-        output.extend(render_archive(definition['pages']))
-    elif definition['type'] == 'directory_archive':
+    for line in [' ', [definition['preamble']] , ' ']:
+        output.extend(line)
+
+    if definition['type'] == 'directory_archive':
         output.extend(render_directory_archive(definition['directory'], output_type='archive'))
     elif definition['type'] == 'directory_include':
         output.extend(render_directory_archive(definition['directory'], output_type='include'))
+    else: 
+        output.extend(render_content(definition['pages'], definition['type']))
 
     output.append(definition['postamble'])
 
@@ -91,6 +84,12 @@ def get_output_file(name, input_file):
     else:
         return name
 
+def parse_page(input):
+    return yaml.load_all(open(input, 'r').read()).next()
+
+def parse_meta(input_page, yaml_extension='yaml'):
+    return yaml.load(open(input_page.rsplit('.', 1)[0] + '.' + yaml_extension, 'r').read())
+
 def cli():
     parser = argparse.ArgumentParser('build rst page from a yaml spec')
     parser.add_argument('input', nargs='?', help='specify input file.' )
@@ -101,8 +100,8 @@ def cli():
 def main():
     interface = cli()
 
-    write_file( get_output_file(interface.output, interface.input),
-                render(parse_page(interface.input)) )
+    write_file( output_file=get_output_file(interface.output, interface.input),
+                content=render(parse_page(interface.input)) )
 
 if __name__ == '__main__':
     main()
